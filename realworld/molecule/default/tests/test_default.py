535bc9967c4dfa095404a762c13c7c6d6d4d4017
pnoc <pnoctulle.pro@gmail.com>
2019-02-26 15:10:53 +0100

    add verify assertions

--- realworld/molecule/default/tests/test_default.py
+++ realworld/molecule/default/tests/test_default.py
@@ -12,3 +12,16 @@ def test_hosts_file(host):
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+
+def test_user_and_group_exists(host):
+    grp = host.group('realworld')
+    assert grp != None
+    usr = host.user('realworld')
+    assert usr != None
+    assert 'realworld' in usr.groups
+    assert usr.home == '/home/realworld'
+
+def test_service(host):
+    srv = host.service("realworld-server")
+    assert srv.is_running
+    assert srv.is_enabled
