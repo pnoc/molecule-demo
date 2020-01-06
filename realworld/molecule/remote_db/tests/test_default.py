1a4ee1c0df6ffe28a67bbea4fdb10cff2fa8ed3c
pnoc <pnoctulle.pro@gmail.com>
2020-01-06 22:38:20 +0100

    fix remote_db scenario

--- realworld/molecule/remote_db/tests/test_default.py
+++ realworld/molecule/remote_db/tests/test_default.py
@@ -12,3 +12,23 @@ def test_hosts_file(host):
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+
+def test_user_and_group_exists(host):
+    grp = host.group('realworld')
+    assert grp is not None
+    usr = host.user('realworld')
+    assert usr is not None
+    assert 'realworld' in usr.groups
+    assert usr.home == '/home/realworld'
+
+
+def test_service(host):
+    srv = host.service("realworld-server")
+    assert srv.is_running
+    assert srv.is_enabled
+
+
+def test_api(host):
+    cmd = host.run('curl -so /dev/null -w "%{http_code}" http://localhost:3000/api/articles')
+    assert cmd.rc == 0
+    assert cmd.stdout == '200'
